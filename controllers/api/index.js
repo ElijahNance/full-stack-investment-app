4201aeb2582d1ebe4bdcf532cac04b49a44b762d
unknown <jesseelijah.nance@gmail.com>
2023-01-19 14:50:43 -0500

    got login/log out button working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const stockRoutes = require('./stock-routes.js');
 
 router.use('/users', userRoutes);
-
+router.use('/stocks', stockRoutes);
 module.exports = router;

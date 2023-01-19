98215bde9be2fc1b34bae570ac6fc1c3f75c7ac7
unknown <jesseelijah.nance@gmail.com>
2023-01-19 13:24:04 -0500

    created stock model, created get routes for user/stock

---
+++ models/Stock.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Stock extends Model {
+
+}
+
+Stock.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        stock_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        investor_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'stock'
+    },
+)
+
+module.exports = Stock;
